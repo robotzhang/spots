eb53dcc05af878572169475a5047ea839b154def
robotzhang <445679586@qq.com>
2012-09-28 20:07:15 +0800

    验证框架信息

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['required']			= "%s不能为空.";
+$lang['isset']				= "必须设置%s的值.";
+$lang['valid_email']		= "%s不是一个有效的邮箱地址.";
+$lang['valid_emails']		= "The %s field must contain all valid email addresses.";
+$lang['valid_url']			= "%s不是一个有效的URL地址.";
+$lang['valid_ip']			= "%s不是一个正确的IP地址.";
+$lang['min_length']			= "%s长度不能少于%s.";
+$lang['max_length']			= "%s长度必须大于%s.";
+$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['alpha']				= "%s只能包含字母.";
+$lang['alpha_numeric']		= "%s只能包含字母和数字.";
+$lang['alpha_dash']			= "%s只能包含字母/数字/下划线/破折号.";
+$lang['numeric']			= "%s只能包含数字.";
+$lang['is_numeric']			= "%s只能是数字.";
+$lang['integer']			= "%s只能是整数.";
+$lang['regex_match']		= "The %s field is not in the correct format.";
+$lang['matches']			= "%s和%s不一致.";
+$lang['is_natural']			= "%s必须是自然数.";
+$lang['is_natural_no_zero']	= "%s必须是大于0的自然数.";
+$lang['decimal']			= "%s必须包含小数.";
+$lang['less_than']			= "%s必须小于%s.";
+$lang['greater_than']		= "%s必须大于%s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
