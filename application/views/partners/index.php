a7ff786bd2cdbab995cfe73634c3dd829b87969f
robotzhang <445679586@qq.com>
2012-10-01 22:20:05 +0800

    重新调整了合作景区首页

--- application/views/partners/index.php
+++ application/views/partners/index.php
@@ -1,2 +1,2 @@
-<p>你有<a href="<?php echo site_url('my/homework'); ?>">5</a>条课后作业待批阅</p>
-<p>有<a href="<?php echo site_url('my/students'); ?>">3</a>位新同学加入了“来，我们来学php”课堂</p>
+<p>近一周(2012/10/1 - 2012/10/1)门票售出情况</p>
+<div>共售出 3 张</div>
