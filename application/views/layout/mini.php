ac19a50b8628e89a7961730117e729de67a1f7a4
robotzhang <445679586@qq.com>
2012-10-01 13:18:03 +0800

    优化mini模块的设置

--- application/views/layout/mini.php
+++ application/views/layout/mini.php
@@ -10,6 +10,12 @@
 </head>
 
 <body>
-    <?php echo $layout['content']; ?>
+    <div class="site">
+        <?php echo $layout['content']; ?>
+    </div>
+    <div class="footer">
+        ©2012 uto168.com All rights reserved.
+        <a href="<?php echo site_url() ?>">返回网站首页</a>
+    </div>
 </body>
 </html>
--- application/views/partners/login.php
+++ application/views/partners/login.php
@@ -1,22 +1,16 @@
 <?php $this->layout->setLayout(array('title' => '景区合作伙伴 -- 登录')); ?>
-<div class="site">
-    <div class="users">
-        <form method="post" action="<?php echo site_url('partners/login') ?>">
-            <div class="item">
-                <label>账号</label>
-                <input type="text" name="partner[name]" value="">
-            </div>
-            <div class="item">
-                <label>密码</label>
-                <input type="password" name="partner[password]" value="">
-            </div>
-            <div class="item">
-                <button class="btn" style="margin-left: 38px;" type="submit">登录</button>
-            </div>
-        </form>
-    </div>
-</div>
-<div class="footer">
-    ©2012 uto168.com All rights reserved.
-    <a href="<?php echo site_url() ?>">返回网站首页</a>
+<div class="users">
+    <form method="post" action="<?php echo site_url('partners/login') ?>">
+        <div class="item">
+            <label>账号</label>
+            <input type="text" name="partner[name]" value="">
+        </div>
+        <div class="item">
+            <label>密码</label>
+            <input type="password" name="partner[password]" value="">
+        </div>
+        <div class="item">
+            <button class="btn" style="margin-left: 38px;" type="submit">登录</button>
+        </div>
+    </form>
 </div>
