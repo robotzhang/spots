a89b98acbb87832c21fc2cac81cda965f3fdb58e
robotzhang <445679586@qq.com>
2012-10-02 16:45:34 +0800

    重新调整用户主页的时光轴

--- application/views/my/index.php
+++ application/views/my/index.php
@@ -5,7 +5,7 @@
         <div class="item_box">
             <div class="pop-triangle"></div>
             <div>
-                <div class="date"><?php echo date('Y-m-d', strtotime(current_user()->created_at)) ?></div>
+                <span class="date"><?php echo date('Y-m-d', strtotime(current_user()->created_at)) ?></span>
                 成功注册
             </div>
         </div>
@@ -15,7 +15,7 @@
         <div class="item_box">
             <div class="pop-triangle"></div>
             <div>
-                <div class="date"><?php echo date('Y-m-d', strtotime($handbook->updated_at)) ?></div>
+                <span class="date"><?php echo date('Y-m-d', strtotime($handbook->updated_at)) ?></span>
                 成功激活景点手册<span class="red"><?php echo $handbook->unique_id ?></span>
             </div>
         </div>
@@ -26,8 +26,8 @@
         <div class="item_box">
             <div class="pop-triangle"></div>
             <div>
-                <div class="date"><?php echo date('Y-m-d', strtotime($ticket->created_at)) ?></div>
-                成功购买<span class="red"><?php echo $ticket->spot->name ?></span>景区门票
+                <span class="date"><?php echo date('Y-m-d', strtotime($ticket->created_at)) ?></span>
+                成功购买<a target="_blank" href="<?php echo site_url('spots/show/'.$ticket->spot->id) ?>" class="red"><?php echo $ticket->spot->name ?></a>景区门票
             </div>
         </div>
     </div>
