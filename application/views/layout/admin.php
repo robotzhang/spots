99f93727fcac78df49588c55c0a2f78562675846
robotzhang <445679586@qq.com>
2012-10-07 14:14:20 +0800

    将远程css调用改成本地css和jquery调用

--- application/views/layout/admin.php
+++ application/views/layout/admin.php
@@ -10,9 +10,9 @@ $action_next = $this->uri->segment(2);
   <meta name="keywords" content="<?php echo $layout['keywords']; ?>" />
   <title><?php echo $layout['title']; ?></title>
   <link href="<?php echo base_url('static/images/favicon.png'); ?>" type="image/x-icon" rel="icon">
-  <link rel="stylesheet" href="http://lib.sinaapp.com/js/bootstrap/2.0.3/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="<?php echo base_url('static/stylesheets/application.css') ?>" />
   <link rel="stylesheet" href="<?php echo base_url('static/stylesheets/global.css'); ?>" />
-  <script src="http://lib.sinaapp.com/js/jquery/1.8/jquery.min.js"></script>
+  <script src="<?php echo base_url('static/javascripts/jquery.js') ?>"></script>
   <?php
   if (!empty($layout['css'])) {
       foreach ($layout['css'] as $css) {
