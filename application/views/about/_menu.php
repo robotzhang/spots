9ebe2cfb258c87ff28e2a470118224f280cc5dcf
robotzhang <445679586@qq.com>
2012-10-13 23:35:17 +0800

    关于我们加上图标优化效果

--- application/views/about/_menu.php
+++ application/views/about/_menu.php
@@ -1,7 +1,18 @@
-<ul class="nav nav-tabs nav-stacked">
-    <li><a href="<?php echo site_url('about/active') ?>">活动介绍</a></li>
-    <li><a href="<?php echo site_url('about/lines') ?>">线路推介</a></li>
-    <li><a href="<?php echo site_url('about/tools') ?>">旅行工具箱</a></li>
-    <li><a href="<?php echo site_url('about/books') ?>">自驾游路书</a></li>
-    <li><a href="<?php echo site_url('about/company') ?>">公司简介</a></li>
+<?php $action_next = $this->uri->segment(2); ?>
+<ul class="nav nav-tabs nav-stacked" style="margin-right:  20px;">
+    <li<?php if($action_next=='active') echo ' class="active"' ?>>
+        <a href="<?php echo site_url('about/active') ?>"><i class="icon-flag"></i> 活动介绍</a>
+    </li>
+    <li<?php if($action_next=='lines') echo ' class="active"' ?>>
+        <a href="<?php echo site_url('about/lines') ?>"><i class="icon-road"></i> 线路推介</a>
+    </li>
+    <li<?php if($action_next=='tools') echo ' class="active"' ?>>
+        <a href="<?php echo site_url('about/tools') ?>"><i class="icon-tag"></i> 旅行工具箱</a>
+    </li>
+    <li<?php if($action_next=='books') echo ' class="active"' ?>>
+        <a href="<?php echo site_url('about/books') ?>"><i class="icon-book"></i> 自驾游路书</a>
+    </li>
+    <li<?php if($action_next=='company') echo ' class="active"' ?>>
+        <a href="<?php echo site_url('about/company') ?>"><i class="icon-info-sign"></i> 公司简介</a>
+    </li>
 </ul>
