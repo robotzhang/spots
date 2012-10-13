a35e06237691e4288043d94b188e139d05f84d96
robotzhang <445679586@qq.com>
2012-10-13 18:55:07 +0800

    增加显示手册的激活状态显示

--- application/views/admin/handbooks/index.php
+++ application/views/admin/handbooks/index.php
@@ -10,6 +10,7 @@
         <tr>
             <th width="40">id</th>
             <th>手册ID</th>
+            <th>状态</th>
             <th>创建时间</th>
             <th>操作</th>
         </tr>
@@ -19,6 +20,9 @@
         <tr>
             <td><?php echo $handbook->id ?></td>
             <td><?php echo $handbook->unique_id ?></td>
+            <td>
+                <?php echo $handbook->is_used == 'Y' ? '已使用' : '未激活' ?>
+            </td>
             <td><?php echo $handbook->created_at ?></td>
             <td>
                 <a title="删除" onclick="if(!confirm('确定删除？'))return false;" href="<?php echo site_url('admin/handbooks/delete?id='.$handbook->id) ?>"><i class="icon-trash"></i></a>
