b17a9124eea56009df64264ea5d2c58e46029aff
robotzhang <445679586@qq.com>
2012-11-27 11:44:02 +0800

    批量生成sql脚本

---
+++ sql/sql.rb
@@ -0,0 +1,6 @@
+﻿require 'rubygems'
+file = File.new("sql.sql", "a")
+(1000001..1002000).each do |id|
+  sql = sprintf("INSERT INTO `handbooks` SET `unique_id`='NO.CN%d', `is_used`='N', `created_at`=NOW(),  `updated_at`=NOW();", id)
+  file.puts(sql)
+end
